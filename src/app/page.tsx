c9e0f73392ad228be2c318157c68530e14431657
Pedro Pimpão <pedrohmpimpao@gmail.com>
2025-10-20 15:19:47 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from "./_components/ui/button";
+
 export default function HomePage(){
   return (
     <>
       <h1 className="text-red-500 bg-red-400 mb-5">Home Page</h1>
-      
+      <Button variant="default" size="lg">
+        Click Me
+      </Button>
     </>
   )
 }

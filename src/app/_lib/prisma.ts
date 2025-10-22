09e3d7d37a7bf3937fd08fd516065081fe6a106b
Pedro Pimpão <pedrohmpimpao@gmail.com>
2025-10-22 17:42:31 -0300

    refactor: remove commented excerpt in prisma middleware

--- src/app/_lib/prisma.ts
+++ src/app/_lib/prisma.ts
@@ -1,22 +1,4 @@
 import { PrismaClient } from "../../generated/prisma"
-// import { PrismaClient } from "@prisma/client"
-
-// declare global {
-//   // eslint-disable-next-line no-unused-vars
-//   var cachedPrisma: PrismaClient | undefined
-// }
-
-// let prisma: PrismaClient
-// if (process.env.NODE_ENV === "production") {
-//   prisma = new PrismaClient()
-// } else {
-//   if (!global.cachedPrisma) {
-//     global.cachedPrisma = new PrismaClient()
-//   }
-//   prisma = global.cachedPrisma
-// }
-
-// export const db = prisma
 
 let prisma: PrismaClient
 
